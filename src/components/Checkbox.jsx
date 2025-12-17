b628321b7b5ca0e353de5b24207943d46f1b0a31
Lucas Baccelli <94851988+baxelin@users.noreply.github.com>
2025-12-17 10:49:36 -0300

    style: add checkbox style

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,10 +1,11 @@
+import "./Checkbox.css"
+
 function Checkbox () {
     return (
-        <>
-            <br />
+        <div className="checkbox-group">
             <input type="checkbox" id="checkbox" name="checkbox"/>
             <label htmlFor="checkbox">I consent to being contacted by the team. *</label>
-        </>
+        </div>
     )
 }
 
