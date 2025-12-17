89576554ef5fec82188efd1a12e89c7dc01b4d7f
Lucas Baccelli <94851988+baxelin@users.noreply.github.com>
2025-12-17 09:59:28 -0300

    style: add style

--- src/components/RadioGroup.jsx
+++ src/components/RadioGroup.jsx
@@ -1,13 +1,18 @@
+import "./RadioGroup.css"
+
 function RadioGroup () {
     return (
-        <>
-            <label htmlFor="">Query Type *</label><br />
-            <input type="radio" id="general" name="queryType" value="general" />
-            <label htmlFor="general">General Enquiry</label>
-            <input type="radio" id="support" name="queryType" value="support" />
-            <label htmlFor="support">Support Request</label>
-            <br />
-        </>
+        <div className="radio-group">
+            <label htmlFor="">Query Type *</label>
+            <div className="flex-start">
+                <input type="radio" id="general" name="queryType" value="general" />
+                <label htmlFor="general">General Enquiry</label>  
+            </div>
+            <div className="flex-start">
+                <input type="radio" id="support" name="queryType" value="support" />
+                <label htmlFor="support">Support Request</label>                
+            </div>
+        </div>
     )
 }
 
--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,13 +1,15 @@
+import "./TextInput.css";
+
 function TextInput () {
     return (
-        <>
-            <label htmlFor="first-name">First Name *</label><br/>
-            <input type="text" id="first-name" name="first-name" required/><br />
-            <label htmlFor="last-name">Last Name *</label><br/>
-            <input type="text" id="last-name" name="last-name" required/><br />
-            <label htmlFor="email">E-mail *</label><br/>
-            <input type="email" id="email" name="email" required/><br />
-        </>
+        <div className="input-text">
+            <label htmlFor="first-name">First Name *</label>
+            <input type="text" id="first-name" name="first-name" required/>
+            <label htmlFor="last-name">Last Name *</label>
+            <input type="text" id="last-name" name="last-name" required/>
+            <label htmlFor="email">E-mail Address *</label>
+            <input type="email" id="email" name="email" required/>
+        </div>
     )
 }
 
